f6dbd2bd0c2259eca0e97a4ea7574bd5d2e48f31
Faire90 <Faire90@users.noreply.github.com>
2021-04-07 10:41:49 -0700

    Die side functionality and Error checking
    Added the ability to decide how many sides the "die" being rolled has.
    Added error checking so it doesn't crash if you don't enter an integer.

--- Main.py
+++ Main.py
@@ -2,16 +2,17 @@ import random
 
 totalStreaks = 0
 n = 0
+dieSides = 0
+flipNumber = 0
+streakLengths = 0
+sets = 0
 
-def coinFlips(number):
+def coinFlips(sides, number):
     flips = []
     i = 0
     while i < number:
-        flip = random.randint(0, 1)
-        if flip == 0:
-            flips.append('H')
-        if flip == 1:
-            flips.append('T')
+        flip = random.randint(0, (sides - 1))
+        flips.append(flip)
         i += 1
     return flips
 
@@ -29,16 +30,57 @@ def checkRepeats(list, streakLength):
             streak = 0
             repeats += 1
     return repeats
+while True:
+    print("How many sides should the die have?")
+    while True:
+        try:
+            dieSides = int(input())
+            break
+        except ValueError:
+            print('please enter an integer.')
+    if dieSides == 2:
+        while True:
+            try:
+                print("How many coin flips per set?")
+                flipNumber = int(input())
+                break
+            except ValueError:
+                print('please enter an integer.')
+    elif dieSides == 1:
+        while True:
+            try:
+                print("How many times do you want to repeat 1?")
+                flipNumber = int(input())
+                break
+            except ValueError:
+                print('please enter an integer.')
+    else:
+        while True:
+            try:
+                print("How many die rolls per set?")
+                flipNumber = int(input())
+                break
+            except ValueError:
+                print('please enter an integer.')
+    while True:
+        try:
+            print("How long should the streak be?")
+            streakLengths = int(input())
+            break
+        except ValueError:
+            print('please enter an integer.')
 
-print("How many coin flips per set?")
-flipNumber = int(input())
-print("How long should the streak be?")
-streakLengths = int(input())
-print("How many sets?")
-sets = int(input())
+    while True:
+        try:
+            print("How many sets?")
+            sets = int(input())
+            break
+        except ValueError:
+            print('please enter an integer.')
+    break
 
 while n < sets:
-    lists = coinFlips(flipNumber)
+    lists = coinFlips(dieSides, flipNumber)
     repeat = checkRepeats(lists, streakLengths)
     if repeat > 0:
         totalStreaks += 1
@@ -46,6 +88,6 @@ while n < sets:
         pass
     n += 1
 
-print("The number of", flipNumber, "flip sets with a streak of ", streakLengths, " or more is:", totalStreaks)
+print("The number of", flipNumber, "roll sets with a streak of ", streakLengths, " or more is:", totalStreaks)
 percentage = totalStreaks/sets
 print("The percentage of sets with a streak of", streakLengths, "or more is:", (percentage * 100), "%")
